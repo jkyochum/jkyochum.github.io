bd25d9eb519bc941a4a08880fd1e086497d4f093
Jeremy Yochum <jyochum391@yahoo.com>
2023-09-07 04:28:17 -0400

    create resume modal / add click functionality to enter and exit modal

--- js/main.js
+++ js/main.js
@@ -7,20 +7,53 @@ const burger = document.querySelector('.navLine');
 const welcomeWrapper = document.querySelector('#welcomeWrapper');
 const scrollArrow = document.querySelector('#scrollArrow');
 const projects = document.querySelector('#projects');
+const resumeModal = document.querySelector('#resumeModal');
+const resume = document.querySelector('.resume');
+const fixedMenu = document.querySelector('#fixedMenu');
+const welcomeResume = document.querySelector('#welcomeResume');
+const footerNav = document.querySelector('#footerNav');
+const footerResume = document.querySelector('#resume');
 
 //Event Handlers
 
 btnNavToggle.addEventListener('click', toggleNav);
 
+scrollArrow.addEventListener('click', arrowScroll);
+
 slidingMenu.addEventListener('click', (e) => {
     console.log(e);
     const className = e.target.parentElement.className;
+    const parentId = e.target.parentElement.id;
     if (className === 'navLink') {
         toggleNav();
     }
+
+    if (parentId === 'headerResume') {
+        resumeModal.classList.remove('closed');
+        toggleScrollbar(true);
+        html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+
+    }
 });
 
-scrollArrow.addEventListener('click', arrowScroll);
+resumeModal.addEventListener('click', (e) => {
+    closeResumeModal(e);
+});
+
+footerNav.addEventListener('click', (e) => {
+    if (e.target.parentElement.parentElement.parentElement.parentElement.id === 'resume' || e.target.parentElement.parentElement.parentElement.id === 'resume' || e.target.parentElement.parentElement.id === 'resume' || e.target.parentElement.id === 'resume' || e.target.id === 'resume') {
+        resumeModal.classList.remove('closed');
+        toggleScrollbar(true);
+    }
+});
+
+fixedMenu.addEventListener('click', (e) => {
+    const parentId = e.target.parentElement.id;
+    if (parentId === 'welcomeResume') {
+        resumeModal.classList.remove('closed');
+        toggleScrollbar(true);
+    }
+})
 
 document.addEventListener('scroll', (e) => {
     if (window.scrollY > 90) {
@@ -69,6 +102,12 @@ function toggleNav() {
     }
 }
 
+function closeResumeModal(e) {
+    if (e.target.parentElement.id !== 'resumeCard') {
+        resumeModal.classList.add('closed');
+        toggleScrollbar(false);
+    }
+}
 
 // window.onload = (e) => {
 //     window.scrollY = 0;
