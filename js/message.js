8843865b1c3318cc485891a77ef11ad3efdf70e2
Jeremy Yochum <jyochum391@yahoo.com>
2023-09-11 21:43:45 -0400

    add SMTPJS link and Email function

--- js/message.js
+++ js/message.js
@@ -13,7 +13,20 @@ const fields = {};
 //     .catch(err => console.error(err));
 
 
-
+//main function
+function sendMessage() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "username",
+        Password: "password",
+        To: 'them@website.com',
+        From: "you@isp.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
 
 
 
