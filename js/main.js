d19eb0b4070bdeacafd11a8c2638fe36b5e2fad0
Jeremy Yochum <jyochum391@yahoo.com>
2023-09-24 00:06:46 -0400

    finish accessible tabbing for header menu and footer menu / working on tabbing for welcome menu

--- js/main.js
+++ js/main.js
@@ -13,11 +13,18 @@ const contactExit = document.querySelector('#contactExitBtn');
 // const messageSentCard = document.querySelector('#messageSent');
 const messageSentExit = document.querySelector('#messageSentExitBtn');
 const resumeModal = document.querySelector('#resumeModal');
+const resumeExit = document.querySelector('#resumeExitBtn');
+const downloadLink = document.querySelector('#downloadLink a');
 const resume = document.querySelector('.resume');
 const fixedMenu = document.querySelector('#fixedMenu');
+const welcomeContact = document.querySelector('#welcomeContact');
 const welcomeResume = document.querySelector('#welcomeResume');
 const footerNav = document.querySelector('#footerNav');
-const footerResume = document.querySelector('#resume');
+const footerContact = document.querySelector('#footerContact');
+const footerResume = document.querySelector('#footerResume');
+let headerLinkActive = false;
+let welcomeLinkActive = false;
+let footerLinkActive = false;
 
 
 //Event Handlers
@@ -36,7 +43,7 @@ slidingMenu.addEventListener('click', (e) => {
     }
 
     //clicking inside of sliding menu empty space will trigger toggle button focus
-    if (target.id === 'slidingMenu' || e.target.tagName === 'LI' || e.target.tagName === 'UL') {
+    if (target.id === 'slidingMenu' || target.tagName === 'LI' || target.tagName === 'UL') {
         btnNavToggle.focus();
     }
 
@@ -44,21 +51,46 @@ slidingMenu.addEventListener('click', (e) => {
         contactModal.classList.remove('closed');
         toggleScrollbar(true);
         html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+        headerLinkActive = true;
+        welcomeLinkActive = false;
+        footerLinkActive = false;
     }
 
     if (target.id === 'headerResume') {
         resumeModal.classList.remove('closed');
         toggleScrollbar(true);
         html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+        headerLinkActive = true;
+        welcomeLinkActive = false;
+        footerLinkActive = false;
+    }
+});
+
+//Opening links on the fixed welcome menu
+fixedMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.id === 'welcomeContact') {
+        contactModal.classList.remove('closed');
+        toggleScrollbar(true);
+        html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+        headerLinkActive = false;
+        welcomeLinkActive = true;
+        footerLinkActive = false;
+    }
+
+    if (target.id === 'welcomeResume') {
+        resumeModal.classList.remove('closed');
+        toggleScrollbar(true);
+        html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+        headerLinkActive = false;
+        welcomeLinkActive = true;
+        footerLinkActive = false;
     }
 });
 
 contactModal.addEventListener('click', (e) => {
     closeContactModal(e);
     const target = e.target;
-
-    // console.log(e);
-    console.log(target);
 
     //set focus on exit button if any empty space is clicked on contact form
     if (target.id === 'contactHeader' || target.id === 'contactForm' || target.id === 'contactCard') {
@@ -71,22 +103,15 @@ contactModal.addEventListener('click', (e) => {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 });
 
 resumeModal.addEventListener('click', (e) => {
     closeResumeModal(e);
+    const target = e.target;
+
+    if (target.tagName === 'IMG') {
+        resumeExit.focus();
+    }
 });
 
 //Click events for the footer SVG elements
@@ -95,28 +120,18 @@ footerNav.addEventListener('click', (e) => {
         resumeModal.classList.remove('closed');
         toggleScrollbar(true);
         html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+        headerLinkActive = false;
+        welcomeLinkActive = false;
+        footerLinkActive = true;
     }
 
     if (e.target.parentElement.parentElement.parentElement.parentElement.id === 'footerContact' || e.target.parentElement.parentElement.parentElement.id === 'footerContact' || e.target.parentElement.parentElement.id === 'footerContact' || e.target.parentElement.id === 'footerContact' || e.target.id === 'footerContact') {
         contactModal.classList.remove('closed');
         toggleScrollbar(true);
         html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
-    }
-});
-
-//Opening links on the fixed welcome menu
-fixedMenu.addEventListener('click', (e) => {
-    const target = e.target;
-    if (target.id === 'welcomeContact') {
-        contactModal.classList.remove('closed');
-        toggleScrollbar(true);
-        html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
-    }
-
-    if (target.id === 'welcomeResume') {
-        resumeModal.classList.remove('closed');
-        toggleScrollbar(true);
-        html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+        headerLinkActive = false;
+        welcomeLinkActive = false;
+        footerLinkActive = true;
     }
 });
 
@@ -140,12 +155,9 @@ document.addEventListener('keydown', (e) => {
     const target = e.target;
     checkForOpenHeader();
 
-    console.log('Contact form keydown (e): ');
-    console.log(e);
-    console.log('Contact form activeElement: ');
-    console.log(document.activeElement);
-
     if (e.key === 'Enter') {
+        console.log(target.parentElement);
+
 
         if (target.className === 'navLink') {
             toggleNav();
@@ -163,23 +175,68 @@ document.addEventListener('keydown', (e) => {
             contactModal.classList.remove('closed');
             toggleScrollbar(true);
             html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
-        }
-
-        if (target === contactExit) {
-            closeContactModal(e);
-            btnNavToggle.focus();
-        }
-
-        if (target === messageSentExit) {
-            closeContactModal(e);
-            btnNavToggle.focus();
+            headerLinkActive = true;
+            welcomeLinkActive = false;
+            footerLinkActive = false;
         }
 
         if (target.id === 'headerResume') {
             resumeModal.classList.remove('closed');
             toggleScrollbar(true);
             html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+            headerLinkActive = true;
+            welcomeLinkActive = false;
+            footerLinkActive = false;
         }
+
+        if (target.id === 'welcomeContact') {
+            contactModal.classList.remove('closed');
+            toggleScrollbar(true);
+            html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+            headerLinkActive = false;
+            welcomeLinkActive = true;
+            footerLinkActive = false;
+        }
+
+        if (target.id === 'welcomeResume') {
+            resumeModal.classList.remove('closed');
+            toggleScrollbar(true);
+            html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+            headerLinkActive = false;
+            welcomeLinkActive = true;
+            footerLinkActive = false;
+        }
+
+        if (target.parentElement.id === 'footerContact') {
+            contactModal.classList.remove('closed');
+            toggleScrollbar(true);
+            html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+            headerLinkActive = false;
+            welcomeLinkActive = false;
+            footerLinkActive = true;
+        }
+
+        if (target.parentElement.id === 'footerResume') {
+            resumeModal.classList.remove('closed');
+            toggleScrollbar(true);
+            html.style.backgroundColor = 'rgba(0, 0, 0, 0.8';
+            headerLinkActive = false;
+            welcomeLinkActive = false;
+            footerLinkActive = true;
+        }
+
+        if (target === contactExit) {
+            closeContactModal(e);
+        }
+
+        if (target === messageSentExit) {
+            closeContactModal(e);
+        }
+
+        if (target === resumeExit) {
+            closeResumeModal(e);
+        }
+
     }
 
 
@@ -231,14 +288,12 @@ document.addEventListener('keydown', (e) => {
 
         //making contact page tabbable upon opening
         if (!contactCard.classList.contains('closed')) {
-            const contactExit = document.querySelector('#contactExitBtn');
             const contactForm = document.querySelector('#contactForm');
             contactExit.setAttribute('tabindex', '0');
             if (e.key === 'Tab') {
                 e.preventDefault();
-                console.log(activeElement);
 
-                if (activeElement.classList.contains('navLink') || activeElement === contactExit) {
+                if (activeElement.classList.contains('navLink') || activeElement.parentElement === footerContact || activeElement === contactExit) {
                     contactForm.firstElementChild.focus();
                 } else if (activeElement === contactForm.lastElementChild) {
                     contactExit.focus();
@@ -249,9 +304,11 @@ document.addEventListener('keydown', (e) => {
 
             if (e.shiftKey) {
                 if (e.key === 'Tab') {
+
+                    console.log(activeElement);
                     if (activeElement === contactExit) {
                         contactForm.lastElementChild.focus();
-                    } else if (activeElement === contactForm.firstElementChild) {
+                    } else if (activeElement === contactForm.firstElementChild || activeElement.classList.contains('navLink') || activeElement.parentElement.classList.contains('navLink')) {
                         contactExit.focus();
                     } else {
                         activeElement.previousElementSibling.focus();
@@ -269,10 +326,6 @@ document.addEventListener('keydown', (e) => {
 
             if (e.key === 'Tab') {
                 e.preventDefault();
-                console.log('First tab in message sent modal: ');
-                console.log(e);
-                console.log('Active element in message sent: ');
-                console.log(activeElement);
                 if (activeElement.tagName === 'BODY' || activeElement.classList.contains('navLink') || activeElement === messageSentExit) {
                     sendAnotherBtn.focus();
                 } else if (activeElement === sendAnotherBtn) {
@@ -292,21 +345,43 @@ document.addEventListener('keydown', (e) => {
         }
     }
 
+    if (!resumeModal.classList.contains('closed')) {
+        resumeExit.setAttribute('tabindex', '0');
+        downloadLink.setAttribute('tabindex', '0');
 
+        if (e.key === 'Tab') {
+            e.preventDefault();
 
+            if (activeElement.classList.contains('navLink') || activeElement.parentElement === footerResume || activeElement === resumeExit) {
+                downloadLink.focus();
+            } else {
+                resumeExit.focus();
+            }
+        }
 
-
+        if (e.shiftKey) {
+            if (e.key === 'Tab') {
+                if (activeElement === resumeExit) {
+                    downloadLink.focus();
+                } else {
+                    resumeExit.focus();
+                }
+            }
+        }
+    }
 });
 
 //If window size is so large, close the side navigation menu to allow for fixed menu to display
 window.addEventListener('resize', (e) => {
-    let width = document.documentElement.clientWidth + 17;
+    // let width = document.documentElement.clientWidth + 17;
+    let width = e.target.innerWidth;
+    // console.log(e);
 
-    if (slidingMenu.classList.contains('open')) {
-        if (width >= 1024) {
-            toggleNav();
-        }
+    // if (slidingMenu.classList.contains('open')) {
+    if (width > 1023) {
+        toggleNav();
     }
+    // }
 
 });
 
@@ -358,6 +433,14 @@ function toggleNav() {
 
 //Alternate ways to close contact modal screen
 function closeContactModal(e) {
+    if (headerLinkActive) {
+        btnNavToggle.focus();
+    } else if (welcomeLinkActive) {
+        welcomeContact.focus();
+    } else if (footerLinkActive) {
+        footerContact.firstElementChild.focus();
+    }
+
     if (e.target.id === 'contactModal' || e.target.id === 'contactExitBtn' || e.target.id === 'messageSentExitBtn') {
         contactModal.classList.add('closed');
         toggleScrollbar(false);
@@ -366,6 +449,14 @@ function closeContactModal(e) {
 
 //Alternate ways to close resume modal screen
 function closeResumeModal(e) {
+    if (headerLinkActive) {
+        btnNavToggle.focus();
+    } else if (welcomeLinkActive) {
+        welcomeResume.focus();
+    } else if (footerLinkActive) {
+        footerResume.firstElementChild.focus();
+    }
+
     if (e.target.parentElement.id !== 'resumeCard' || e.target.id === 'resumeExitBtn') {
         resumeModal.classList.add('closed');
         toggleScrollbar(false);
